f488c2368a88a005073d867d99f6ac55a5f2df36
henrik.schytze <henrikschytze@gmail.com>
2018-10-07 16:42:52 +0200

    fix the fix :x

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,9 +3,9 @@ import React from "react";
 const Titles = () => (
   <div>
     <h1 className="title-container__title">Weather Finder</h1>
-    <h3 className="title-container__subtitle">
+    <p className="title-container__subtitle">
       Find out temperature, conditions and more...
-    </h3>
+    </p>
   </div>
 );
 
